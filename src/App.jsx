49a1187c7e5c13397719b7c5bd2a56a9a2c65e1e
ayoubsofi21 <ayoubsofi03@gmail.com>
2026-03-04 23:04:48 +0000

    map about all data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import Counter from "./components/Counter.jsx";
+// App.js
+import React from "react";
+import TournamentCard from "./components/TournamentCard"; // Import component
+import { tournamentData } from "./data/tournamentDB"; // Import data
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <>
-      <Counter />
-    </>
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">Good Morning, Samuel Walker!</h1>
+      <input
+        type="search"
+        placeholder="Search"
+        className="px-4 py-2 border border-gray-300 rounded-lg w-full mb-6"
+      />
+      {tournamentData.map((tournament) => (
+        <TournamentCard key={tournament.id} tournament={tournament} />
+      ))}
+      <Footer />
+    </div>
   );
 }
 
